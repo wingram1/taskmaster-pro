9893114bcb28cba6bb58d15d8b1bbd9477fdb1d9
wingram1 <waingram96@gmail.com>
2022-01-18 00:27:30 -0600

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -185,7 +185,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -225,18 +225,20 @@ $(".card .list-group").sortable({
   // creates copy of element and has jQuery move that instead of the original
   helper: "clone",
   //todo delete following educational text
-  // activate: function(event) {
-  //   console.log("activate", this);
-  // },
-  // deactivate: function(event) {
-  //   console.log("deactivate", this);
-  // },
-  // over: function(event) {
-  //   console.log("over", event.target);
-  // },
-  // out: function(event) {
-  //   console.log("out", event.target);
-  // },
+  activate: function(event) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
+  },
+  deactivate: function(event) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
+  },
+  over: function(event) {
+    $(event.target).addClass("dropover-active");
+  },
+  out: function(event) {
+    $(event.target).removeClass("dropover-active");
+  },
   update: function(event) {
     // array to store the task data in
     var tempArr = [];
@@ -273,14 +275,14 @@ $("#trash").droppable({
   accept: ".card .list-group-item",
   tolerance: "touch",
   drop: function(event, ui) {
-    console.log("drop");
+    $(".bottom-trash").removeClass("bottom-trash-active");
     ui.draggable.remove();
   },
   over: function(event, ui) {
-    console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event,ui) {
-    console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 })
 
@@ -303,6 +305,13 @@ var auditTask = function(taskEl) {
   }
 };
 
+//update every 30 minutes
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+  console.log(taskEl);
+}, (1000 * 60) * 30);
 
 
 // load tasks for the first time
